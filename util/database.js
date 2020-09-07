3fdff7e575b5166dae922815f584eff652306cb4
danielezappala <danielezappala@scenariopubblico.com>
2020-09-07 09:18:01 +0200

    second commit

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('testSequelize','root','Scenario_2020',{
     dialect:'mysql', 
-    host: 'localhost',
+    host: 'mysql',
     port:'3306'
 });
 module.exports = sequelize;
